bf5ca00534d96304a2486a1f70b9465ab163b8a7
Hina_Mikan <44318590+RingoCaviar@users.noreply.github.com>
2026-05-11 16:25:15 +0800

    3D视图JS本地化

--- js/features/camera/camera-control-node.js
+++ js/features/camera/camera-control-node.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://unpkg.com/three@0.167.1/build/three.module.js';
+import * as THREE from '../../vendor/three.module.js';
 
 const DEFAULT_CAMERA_STATE = Object.freeze({
     pitch: 12,
